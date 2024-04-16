2a29a456ef2baf56adea127ccbb4b470e75c0f20
Fedev <Fede91@users.noreply.github.com>
2024-04-16 13:00:19 +0200

    improved prompt for nextjs projects

--- src/main.ts
+++ src/main.ts
@@ -79,7 +79,11 @@ async function analyzeCode(
 }
 
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
-  return `Your task is to review pull requests. Instructions:
+  return `You are a senior NextJS developer. Analyze the code's quality and provide suggestions for improvement.
+  Identify common issues such as code smells, anti-patterns, potential bugs, performance bottlenecks, and security vulnerabilities.
+  In the TypeScript files, conduct a thorough check on the variable types. Inline types should not be defined at the time of variable declaration, and the use of "any" or "unknown" must be avoided.
+  Your task is to review pull requests.
+  Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
